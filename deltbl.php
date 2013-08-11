3be639fd9cf65d9b385cad64a45245fc4e7ade3a
Abhishek Potnis <abhishekp.bugzilla@gmail.com>
2013-08-11 23:44:14 +0530

    fixed SQL Injection Vulnerability

--- deltbl.php
+++ deltbl.php
@@ -1,9 +1,12 @@
 <?php
+session_start();
 @include("db.php");
 if(isset($_GET['id'])) {
   $tnm=$_GET['id'];
+  $tnm= base64_decode(str_replace($_SESSION['passkey'],"",$tnm));
   $sql="drop table $tnm";
   mysql_query("$sql");
+  $_SESSION['passkey'] = "";
   header('location:index.php');
 }
 ?>
