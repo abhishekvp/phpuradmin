884bb875c5a815764ce559ffad26c5c7f5db5f21
Abhishek Potnis <abhishekp.bugzilla@gmail.com>
2013-08-14 11:26:05 +0530

    Improved SQL Vulnerability Fix

--- deltbl.php
+++ deltbl.php
@@ -2,11 +2,14 @@
 session_start();
 @include("db.php");
 if(isset($_GET['id'])) {
-  $tnm=$_GET['id'];
-  $tnm= base64_decode(str_replace($_SESSION['passkey'],"",$tnm));
-  $sql="drop table $tnm";
-  mysql_query("$sql");
-  $_SESSION['passkey'] = "";
-  header('location:index.php');
+	$tnm=$_GET['id'];
+	if (strpos($tnm,md5($_SESSION['passkey'])) !== false) {
+		$tnm= base64_decode(str_replace(md5($_SESSION['passkey']),"",$tnm));
+		$sql="drop table $tnm";
+		mysql_query("$sql");
+	}
 }
+$_SESSION['passkey']='';
+header('location:index.php');
 ?>
+
